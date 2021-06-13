4578af0f1b858284bcb82fd4905fb3b94aee200a
JulianaGoyeneche <jgoyeneche42@misena.edu.co>
2021-06-13 13:48:58 -0500

    Carpeta Proyecto NP

---
+++ src/main/java/com/proyectoEjemplo/pyspring/PyspringApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoEjemplo.pyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoEjemplo/pyspring/PyspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoEjemplo.pyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
